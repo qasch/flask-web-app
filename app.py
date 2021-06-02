f916324cdfb5dd0bcca978b3497c92c804db65c0
kschell <kai.schell@lpice.eu>
2021-06-02 14:33:41 +0200

    Print all db entries on index.html

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+import sqlite3
+
+
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    connection = sqlite3.connect('blog.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    c = get_db_connection()
+    posts = c.execute('SELECT * FROM posts').fetchall()
+    c.close()
+    return render_template('index.html', posts_html=posts)
 
 
 @app.route("/wow")
