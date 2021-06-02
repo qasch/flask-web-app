7bde63ba0188f76aefe0ad889fe45fab8d7f409a
kschell <kai.schell@lpice.eu>
2021-06-02 11:07:48 +0200

    Add basic html page with css and image

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello You</h1>"
+def index():
+    return render_template('index.html')
 
 
 @app.route("/wow")
